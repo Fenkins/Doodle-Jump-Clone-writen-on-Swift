1150fa93b5e4770fd86994afa2fce501cfa8f9c8
Anatoly Kovalev <inatoley@gmail.com>
2015-06-10 18:14:01 +0300

    yea

---
+++ Same Doodle Jump Clone writen on Swift/Same Doodle Jump Clone writen on Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Same Doodle Jump Clone writen on Swift
+//
+//  Created by Fenkins on 10/06/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
