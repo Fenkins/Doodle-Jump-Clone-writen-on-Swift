9003f897a135823b3294b16bc405d385467a9e65
Anatoly Kovalev <inatoley@gmail.com>
2015-06-10 18:45:43 +0300

    implement logic, Luke

---
+++ Same Doodle Jump Clone writen on Swift/Same Doodle Jump Clone writen on Swift/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  Same Doodle Jump Clone writen on Swift
+//
+//  Created by Fenkins on 10/06/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    @IBAction func startGameButton(sender: AnyObject) {
+    }
+    @IBOutlet weak var ballImage: UIImageView!
+    @IBOutlet weak var platformImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        platformImage.hidden = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
