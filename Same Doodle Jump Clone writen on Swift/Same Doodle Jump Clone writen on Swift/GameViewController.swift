c02b52c9e51015b6f2a299d7e0c72891c19da5d3
Anatoly Kovalev <inatoley@gmail.com>
2015-06-19 22:32:40 +0300

    changes incoming

--- Same Doodle Jump Clone writen on Swift/Same Doodle Jump Clone writen on Swift/GameViewController.swift
+++ Same Doodle Jump Clone writen on Swift/Same Doodle Jump Clone writen on Swift/GameViewController.swift
@@ -19,12 +19,15 @@ class GameViewController: UIViewController {
     @IBOutlet weak var platformImage4:UIImageView!
     @IBOutlet weak var startGameButtonOut: UIButton!
     
+    var PlatformDropDownFor:CGFloat = 0.0
+    
     var UpMovement:CGFloat = 0
     var SideMovement:CGFloat = 0
     var MoveLeft = false
     var MoveRight = false
     var StopSideMovement = false
     var screenWidth:CGFloat = 0.0
+    var screenHeight:CGFloat = 0.0
     
     let movingConstant:Float = 0.1
     
@@ -42,6 +45,7 @@ class GameViewController: UIViewController {
         
         let screenRect = UIScreen.mainScreen().bounds
         screenWidth = screenRect.width
+        screenHeight = screenRect.height
     }
 
     override func didReceiveMemoryWarning() {
@@ -80,6 +84,7 @@ class GameViewController: UIViewController {
     func Moving() {
         if MoveLeft == true {
             SideMovement -= CGFloat(movingConstant * 3)
+            //Maximum speed is 5
             if SideMovement <= -5 {
                 SideMovement = -5
             }
@@ -99,8 +104,25 @@ class GameViewController: UIViewController {
         
         ballImage.center = CGPointMake(ballImage.center.x + SideMovement, ballImage.center.y - UpMovement)
         UpMovement -= 0.1
-        if (CGRectIntersectsRect(ballImage.frame, platformImage.frame) && (UpMovement <= 0)) {
+        if CGRectIntersectsRect(ballImage.frame, platformImage.frame) && UpMovement <= 0 {
             Bounce()
+            PlatformDropDown()
+        }
+        if CGRectIntersectsRect(ballImage.frame, platformImage1.frame) && UpMovement <= 0 {
+            Bounce()
+            PlatformDropDown()
+        }
+        if CGRectIntersectsRect(ballImage.frame, platformImage2.frame) && UpMovement <= 0 {
+            Bounce()
+            PlatformDropDown()
+        }
+        if CGRectIntersectsRect(ballImage.frame, platformImage3.frame) && UpMovement <= 0 {
+            Bounce()
+            PlatformDropDown()
+        }
+        if CGRectIntersectsRect(ballImage.frame, platformImage4.frame) && UpMovement <= 0 {
+            Bounce()
+            PlatformDropDown()
         }
         PlatformMovement()
     }
@@ -152,13 +174,68 @@ class GameViewController: UIViewController {
     
     // Moving platforms left and right
     func PlatformMovement() {
-        platformImage2.center = CGPointMake(platformImage2.center.x + platform2sideMovement, platformImage2.center.y)
-        platformImage4.center = CGPointMake(platformImage4.center.x + platform4sideMovement, platformImage4.center.y)
+        platformImage.center = CGPointMake(platformImage.center.x, platformImage.center.y + PlatformDropDownFor)
+        platformImage1.center = CGPointMake(platformImage1.center.x, platformImage1.center.y + PlatformDropDownFor)
+        platformImage2.center = CGPointMake(platformImage2.center.x + platform2sideMovement, platformImage2.center.y + PlatformDropDownFor)
+        platformImage3.center = CGPointMake(platformImage3.center.x, platformImage3.center.y + PlatformDropDownFor)
+        platformImage4.center = CGPointMake(platformImage4.center.x + platform4sideMovement, platformImage4.center.y + PlatformDropDownFor)
         //Checking the edges collision
         if platformImage2.center.x >= screenWidth - (platformImage2.bounds.width/2) || platformImage2.center.x <= 0 + (platformImage2.bounds.width/2) {
             platform2sideMovement *= -1
         } else if platformImage4.center.x >= screenWidth - (platformImage4.bounds.width/2) || platformImage4.center.x <= 0 + (platformImage4.bounds.width/2) {
             platform4sideMovement *= -1
+        }
+        //Stopping the platforms falloff
+        PlatformDropDownFor -= CGFloat(movingConstant)
+        if PlatformDropDownFor < 0 {
+            PlatformDropDownFor = 0
+        }
+        //Checking if platform is falled off the screen and bringing platforms back from the top
+        if platformImage.center.y > (screenHeight - platformImage.bounds.size.height) {
+            var randomPosition = CGFloat(arc4random_uniform(UInt32(screenWidth - (platformImage.bounds.size.width/2))))
+            randomPosition += (platformImage.bounds.size.width/2)
+            platformImage.center = CGPointMake(randomPosition, -(platformImage.bounds.size.height/2))
+        }
+        if platformImage1.center.y > (screenHeight - platformImage1.bounds.size.height) {
+            var randomPosition = CGFloat(arc4random_uniform(UInt32(screenWidth - (platformImage1.bounds.size.width/2))))
+            randomPosition += (platformImage1.bounds.size.width/2)
+            platformImage1.center = CGPointMake(randomPosition, -(platformImage1.bounds.size.height/2))
+        }
+        if platformImage2.center.y > (screenHeight - platformImage2.bounds.size.height) {
+            var randomPosition = CGFloat(arc4random_uniform(UInt32(screenWidth - (platformImage2.bounds.size.width/2))))
+            randomPosition += (platformImage2.bounds.size.width/2)
+            platformImage2.center = CGPointMake(randomPosition, -(platformImage2.bounds.size.height/2))
+        }
+        if platformImage3.center.y > (screenHeight - platformImage3.bounds.size.height) {
+            var randomPosition = CGFloat(arc4random_uniform(UInt32(screenWidth - (platformImage3.bounds.size.width/2))))
+            randomPosition += (platformImage3.bounds.size.width/2)
+            platformImage4.center = CGPointMake(randomPosition, -(platformImage3.bounds.size.height/2))
+        }
+        if platformImage4.center.y > (screenHeight - platformImage4.bounds.size.height) {
+            var randomPosition = CGFloat(arc4random_uniform(UInt32(screenWidth - (platformImage4.bounds.size.width/2))))
+            randomPosition += (platformImage4.bounds.size.width/2)
+            platformImage4.center = CGPointMake(randomPosition, -(platformImage4.bounds.size.height/2))
+        }
+    }
+    
+    func PlatformDropDown() {
+        if ballImage.center.y > (screenHeight - 50) {
+            PlatformDropDownFor = 1.0
+        }
+        if ballImage.center.y > (screenHeight - 100) {
+            PlatformDropDownFor = 2.0
+        }
+        if ballImage.center.y > (screenHeight - 150) {
+            PlatformDropDownFor = 3.0
+        }
+        if ballImage.center.y > (screenHeight - 250) {
+            PlatformDropDownFor = 4.0
+        }
+        if ballImage.center.y > (screenHeight - 300) {
+            PlatformDropDownFor = 5.0
+        }
+        if ballImage.center.y > (screenHeight - 400) {
+            PlatformDropDownFor = 6.0
         }
     }
     
